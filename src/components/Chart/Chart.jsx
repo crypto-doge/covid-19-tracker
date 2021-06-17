74d8458c06b8b020b3158efa002e54fcd0e2e087
muraaat <kupenov.murat@smartex.kz>
2021-06-17 23:37:26 +0600

    done card component

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Chart = () => {
-	return (
-		<h1>Chart</h1>
-	)
-}
+  return <h1>Chart</h1>;
+};
 
 export default Chart;
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const CountryPicker = () => {
-	return (
-		<h1>CountryPicker</h1>
-	)
-}
+  return <h1>CountryPicker</h1>;
+};
 
 export default CountryPicker;
